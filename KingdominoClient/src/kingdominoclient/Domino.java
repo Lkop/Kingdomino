6147a1787dfcabf131aa5a07c4ef661b3277aae9
Loukas Kopanias <loukaskopanias122@gmail.com>
2020-07-24 00:11:54 +0300

    Updating models

--- KingdominoClient/src/kingdominoclient/Domino.java
+++ KingdominoClient/src/kingdominoclient/Domino.java
@@ -4,15 +4,36 @@ package kingdominoclient;
 public class Domino {
     
     private int id;
-    private String left;
-    private String right;
-    private int crowns;
+    private Tile tile_1;
+    private Tile tile_2;
+    private Orientation orientation = Orientation.LEFT_RIGHT;
     
-    public Domino(int id, String left, String right, int crowns){
+    public Domino(int id, Tile tile_1, Tile tile_2){
         this.id = id;
-        this.left = left;
-        this.right = right;
-        this.crowns = crowns;
+        this.tile_1 = tile_1;
+        this.tile_2 = tile_2;
     }
     
+    public Tile getTile1() {
+        return this.tile_1;
+    }
+    
+    public Tile getTile2() {
+        return this.tile_2;
+    }
+    
+    public Orientation getOrientation() {
+        return this.orientation;
+    }
+    
+    public void changeOrientationClockwise() {
+        if(orientation == Orientation.LEFT_RIGHT)
+            orientation = Orientation.UP_DOWN;
+        else if(orientation == Orientation.UP_DOWN)
+            orientation = Orientation.RIGHT_LEFT;
+        else if(orientation == Orientation.RIGHT_LEFT)
+            orientation = Orientation.DOWN_UP;
+        else
+            orientation = Orientation.LEFT_RIGHT;
+    }
 }
---
+++ KingdominoClient/src/kingdominoclient/Tile.java
@@ -0,0 +1,22 @@
+package kingdominoclient;
+
+
+public class Tile {
+    
+    private String image;
+    private int crowns;
+    
+    public Tile(String image, int crowns) {
+        this.image = image;
+        this.crowns = crowns;
+    }
+    
+    public String getImage() {
+        return this.image;
+    }
+        
+    public int getCrowns() {
+        return this.crowns;
+    }
+    
+}
