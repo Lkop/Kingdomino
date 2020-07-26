705585534dd0537661b4cc634bb6587020ccbb83
Loukas Kopanias <loukaskopanias122@gmail.com>
2020-07-27 01:52:49 +0300

    Ignore - Clear imports

--- KingdominoClient/src/kingdominoclient/PanelMenu.java
+++ KingdominoClient/src/kingdominoclient/PanelMenu.java
@@ -1,14 +1,11 @@
 package kingdominoclient;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
--- KingdominoClient/src/kingdominoclient/Window.java
+++ KingdominoClient/src/kingdominoclient/Window.java
@@ -1,12 +1,8 @@
 package kingdominoclient;
 
 import java.awt.CardLayout;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 
 public class Window extends JFrame{
